13e5beea5d18abeb9b7401186ba165d9e0f4a845
Kitenge Mukaba Jean-louis <jeanlouismukaba01@yahoo.com>
2023-04-07 14:40:22 +0100

    Import modules

--- src/index.js
+++ src/index.js
@@ -1,24 +1,6 @@
 import './style.css';
+import TaskList from './modules/interactive.js';
 
-const tasks = [
-  { description: 'Attend Morning sesstion', completed: true, index: 1 },
-  { description: 'Pair programming', completed: true, index: 2 },
-  { description: 'Standup meeting', completed: false, index: 3 },
-];
+const taskList = new TaskList();
 
-const todoList = document.getElementById('todo-list');
-
-const renderTasks = () => {
-  tasks.forEach((task) => {
-    const taskCard = document.createElement('div');
-    taskCard.classList = 'task-content';
-    taskCard.innerHTML = `<div class="task-text">
-                            <input class="block" type="checkbox">
-                            <p class="task-text grow">${task.description}</p>
-                          </div>
-                          <i class="fa-solid fa-trash-can" id="delete-task"></i>`;
-    todoList.appendChild(taskCard);
-  });
-};
-
-renderTasks();
+taskList.displayTasks();
