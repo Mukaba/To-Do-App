8dbf7de4bf5ea86d67b603625333d803c77c219e
Kitenge Mukaba Jean-louis <jeanlouismukaba01@yahoo.com>
2023-04-14 21:47:20 +0100

    Update removeTask test function

--- Tests/removeTask.test.js
+++ Tests/removeTask.test.js
@@ -9,4 +9,14 @@ describe('removeTask', () => {
       { id: 2, name: 'Task 3', status: false },
     ]);
   });
+
+  test('not remove any task for missing id', () => {
+    const updatedTasks = removeTask(5, tasks);
+    expect(updatedTasks).toEqual([
+      { id: 1, name: 'Task 1', status: false },
+      { id: 2, name: 'Task 2', status: true },
+      { id: 3, name: 'Task 3', status: false },
+    ]);
+  });
+
 });
