c550da5514c3df5459aff156ef0e90115dcca2c3
Javed-Shahbaz <javedshahbaz007@gmail.com>
2023-04-15 00:36:13 +0500

    update remove function

--- Tests/aremoveTask.test.js
+++ Tests/aremoveTask.test.js
@@ -2,9 +2,9 @@ import removeTask from '../src/modules/removeTask.js';
 import tasks from '../__mock__/localStorage.js';
 
 describe('removeTask', () => {
-  test('should remove task with given id', () => {
-    const updatedTasks = removeTask(2, tasks);
-    expect(updatedTasks).toEqual([
+  test('Remove task using its id', () => {
+    const updateList = removeTask(2, tasks);
+    expect(updateList).toEqual([
       { id: 1, name: 'Task 1', status: false },
       { id: 2, name: 'Task 3', status: false },
     ]);
