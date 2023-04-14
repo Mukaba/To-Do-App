020425575189d37503d5bd6710e1792d330ef9ca
Javed-Shahbaz <javedshahbaz007@gmail.com>
2023-04-15 00:41:22 +0500

    update addTask test

--- Tests/addTask.test.js
+++ Tests/addTask.test.js
@@ -1,12 +1,12 @@
 import Task from '../src/modules/task.js';
 import addTask from '../src/modules/addTask.js';
 
-describe('addTask function', () => {
-  it('add a new task', () => {
+describe('addTask', () => {
+  test('add a new task in list', () => {
     const tasks = [];
     const taskInput = { value: 'Books for children' };
     const result = addTask(taskInput, tasks);
     expect(result).toEqual([new Task('Books for children', false, 1)]);
   });
-  
+
 });
