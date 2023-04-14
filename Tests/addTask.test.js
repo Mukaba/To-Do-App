6b248f5b79ba70cc17683d87f8452aa10a828973
Kitenge Mukaba Jean-louis <jeanlouismukaba01@yahoo.com>
2023-04-14 21:41:02 +0100

    Update addTask test function

--- Tests/addTask.test.js
+++ Tests/addTask.test.js
@@ -2,10 +2,20 @@ import Task from '../src/modules/task.js';
 import addTask from '../src/modules/addTask.js';
 
 describe('addTask', () => {
-  test('add a new task in list', () => {
+  it('add a new task in list', () => {
     const tasks = [];
     const taskInput = { value: 'Books for children' };
     const result = addTask(taskInput, tasks);
     expect(result).toEqual([new Task('Books for children', false, 1)]);
+  });
+
+  it('add a new task for non-empty array', () => {
+    const tasks = [new Task('Books for children', false, 1)];
+    const taskInput = { value: 'Ferry Tails' };
+    const result = addTask(taskInput, tasks);
+    expect(result).toEqual([
+      new Task('Books for children', false, 1),
+      new Task('Ferry Tails', false, 2),
+    ]);
   });
 });
