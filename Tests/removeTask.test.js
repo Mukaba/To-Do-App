7f510b7fceae5c08b506744aceb95e609ba9823a
Javed-Shahbaz <javedshahbaz007@gmail.com>
2023-04-16 00:49:06 +0500

    update removeTask

--- Tests/removeTask.test.js
+++ Tests/removeTask.test.js
@@ -4,18 +4,18 @@ import tasks from '../__mock__/localStorage.js';
 describe('removeTask', () => {
   test('Remove task using its id', () => {
     const updateList = removeTask(2, tasks);
-    expect(updateList).toEqual([
+  expect(updateList).toEqual([
       { id: 1, name: 'Task 1', status: false },
       { id: 2, name: 'Task 3', status: false },
     ]);
   });
 
-  test('for missing ID don't remove any task', () => {
+  test('for missing ID dont remove any task', () => {
     const updateList = removeTask(5, tasks);
     expect(updateList).toEqual([
       { id: 1, name: 'Task 1', status: false },
       { id: 2, name: 'Task 2', status: true },
       { id: 3, name: 'Task 3', status: false },
     ]);
-  });
+});
 });
